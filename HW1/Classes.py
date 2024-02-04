ab8046ba1041c8368e2bde91a62403b2ec0773a3
Alex Lee <arl7816@g.rit.edu>
2024-02-03 23:24:41 -0500

    Documents HW1

--- HW1/Classes.py
+++ HW1/Classes.py
@@ -1,4 +1,7 @@
 class Node:
+    """
+    A single doubly linked node
+    """
     def __init__(self, value: object, left_link: object, right_link) -> None:
         self.value = value
         self.right_link = right_link
@@ -6,28 +9,66 @@ class Node:
         return
     
     def set_right_link(self, link: object) -> None:
+        """
+        sets the right node
+
+        Args:
+            link (object): the new pointee
+        """
         self.right_link = link
         return
     
     def set_left_link(self, link: object) -> None:
+        """
+        Sets the left node
+
+        Args:
+            link (object): the new pointee
+        """
         self.left_link = link
         return
     
     def get_right_link(self) -> object:
+        """
+        Gets the right link of the node
+
+        Returns:
+            object: the right node
+        """
         return self.right_link
     
     def get_left_link(self) -> object:
+        """
+        Gets the left link of the node
+
+        Returns:
+            object: the left node
+        """
         return self.left_link
 
     def get_value(self) -> object:
+        """Gets the current value of the node
+
+        Returns:
+            object: the data value
+        """
         return self.value
 
 class LinkedList:
+    """
+    A linked list data structure
+    """
     def __init__(self) -> None:
         self.size = 0
         self.root = None
 
     def add(self, value: object) -> None:
+        """
+        Adds a value to the linked list
+
+        Args:
+            value (object): the new value
+        """
         self.size += 1
         if self.size == 1:
             self.root = Node(value, None, None)
@@ -36,6 +77,10 @@ class LinkedList:
         self.root = Node(value, None, self.root)
 
     def print_list(self) -> None:
+        """
+        Prints all values in the linked list in the following manner:
+        *start -> ... -> end*
+        """
         statement = ""
         current = self.root
         while current is not None:
@@ -45,20 +90,28 @@ class LinkedList:
         print(statement)
     
 
-
 class Queue:
+    """A queue data structure
+    """
     def __init__(self) -> None:
         self.size = 0
         self.start = None
         self.end = None
 
     def print_queue(self) -> None:
+        """Prints the contents of the queue
+        """
         current = self.end
         while current is not None:
             print(current.get_value())
             current = current.get_right_link()
 
     def enqueue(self, value: object) -> None:
+        """Places a value at the end of the queue
+
+        Args:
+            value (object): the new value
+        """
         self.size += 1
         if self.size == 1:
             self.start = Node(value, None, None)
@@ -70,6 +123,11 @@ class Queue:
         return
     
     def dequeue(self) -> object:
+        """removes the element at the start of the queue
+
+        Returns:
+            object: the element that was removed
+        """
         result = self.start.get_value()
 
         self.size -= 1
@@ -83,7 +141,17 @@ class Queue:
         return result
     
     def peek(self) -> object:
+        """ gets the value of the element at the start of the queue
+
+        Returns:
+            object: the element at the start of the queue
+        """
         return self.start.get_value()
     
     def get_size(self) -> int:
+        """Gets the number of elements within the queue
+
+        Returns:
+            int: the number of elements within the queue
+        """
         return self.size
--- HW1/hw1.py
+++ HW1/hw1.py
@@ -1,7 +1,16 @@
 from Classes import Queue, LinkedList
 import sys
 
-def get_words(path: str) -> set:
+def get_words(path: str) -> set[str]:
+    """
+    Gets the set of all words we are aware of
+
+    Args:
+        path (str): the text file of all words that are given to us
+
+    Returns:
+        set[str]: the set of all words in the text file
+    """
     words = set()
 
     with open(path, "r") as file:
@@ -11,6 +20,16 @@ def get_words(path: str) -> set:
 
 # fix this and it works
 def generate_neigh(word: str) -> list[str]:
+    """
+    Generates all possible neighbors for a given word, where each letter in the word
+    is then replaced by every letter in the alphebet
+
+    Args:
+        word (str): the current word
+
+    Returns:
+        list[str]: all possible words that can be formed by replacing one letter in the word
+    """
     word = list(word)
     words = []
     alphebet = [chr(n) for n in range(ord("a"), ord("z") + 1)]
@@ -24,7 +43,18 @@ def generate_neigh(word: str) -> list[str]:
 
     return words
 
-def construct_path(visited: dict, start: str, goal: str) -> LinkedList:
+def construct_path(visited: dict, start: str, goal: str) -> LinkedList[str]:
+    """
+    Constructs the path using a visited hashmap
+
+    Args:
+        visited (dict): key is the child node, value is the parent node
+        start (str): the initial state
+        goal (str): the end state
+
+    Returns:
+        LinkedList[str]: the path represented by a linked list
+    """
     path = LinkedList()
     path.add(goal)
 
@@ -38,14 +68,25 @@ def construct_path(visited: dict, start: str, goal: str) -> LinkedList:
     
     return path
 
-def find_path(start: str, goal: str, words: set) -> None:
+def find_path(start: str, goal: str, words: set[str]) -> LinkedList[str]:
+    """Finds the path from a start word to some goal word
+
+    Args:
+        start (str): the initial word
+        goal (str): the end goal
+        words (set[str]): all known words
+
+    Returns:
+        LinkedList[str]: the path from one word to another as start -> ... -> end word
+        defaults to None when no path is found
+    """
+
     queue = Queue()
     visited = {}
     current = None
     queue.enqueue(start)
     
     visited[start] = start
-
 
     while True:
         if queue.get_size() == 0:
@@ -60,11 +101,15 @@ def find_path(start: str, goal: str, words: set) -> None:
                 queue.enqueue(wrd)
                 visited[wrd] = current
         
-
     return None
 
-
 def main() -> None:
+    """
+    Performs a BFS search to find the shortest path in changing words from
+    start word to goal word
+
+    @pre-cond: argv = [file path, start word, goal word]
+    """
     start, goal = sys.argv[2], sys.argv[3]
 
     words = get_words(sys.argv[1])
